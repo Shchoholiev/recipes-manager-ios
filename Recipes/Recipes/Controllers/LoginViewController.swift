cb573e1e11c871888c6198aa40a96aaefa2fe286
Shchoholiev <sh.codework@gmail.com>
2023-05-29 00:02:47 -0400

    RM-82 added Login UI

---
+++ Recipes/Recipes/Controllers/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  Recipes
+//
+//  Created by Serhii Shchoholiev on 5/28/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
---
+++ Recipes/Recipes/Controllers/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  Recipes
+//
+//  Created by Serhii Shchoholiev on 5/28/23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
